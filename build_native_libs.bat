4cda46cc303280431b9baafeef42185941f69bd8
Tingfan Wu <twu@ihmc.us>
2014-12-10 14:57:34 -0600

    commit windows binary

--- build_native_libs.bat
+++ build_native_libs.bat
@@ -1,6 +1,7 @@
 @echo open VisualStudioTool\Nativex64 promprt with administrative right from startmenu.
 @echo Please install Visual Studio 2013 Professional (Visual Studio Express can't compile to native x64)
 @echo change JDK home in ..\CMakeLists.txt
+set PATH=%PATH%;c:\swig
 rmdir /s /q  build generated-src
 mkdir build
 cd build
